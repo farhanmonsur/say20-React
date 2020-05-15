2c6d996fa3fdf5f5a3f6d1b14a5ecd8d550b62fb
Farhan Monsur <48153229+farhanmonsur@users.noreply.github.com>
2020-05-15 22:09:03 +0600

    Updates

---
+++ precache-manifest.1c9ac438e7f5cc8172e2e62b856ed221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f36d6a807110ae81bc8a6ece253e959",
+    "url": "/say20-React/index.html"
+  },
+  {
+    "revision": "6fa4ed44e09fb51d7db9",
+    "url": "/say20-React/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "4e31ab78524265a9846f",
+    "url": "/say20-React/static/css/main.2e77fa78.chunk.css"
+  },
+  {
+    "revision": "6fa4ed44e09fb51d7db9",
+    "url": "/say20-React/static/js/2.63443c97.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/say20-React/static/js/2.63443c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e31ab78524265a9846f",
+    "url": "/say20-React/static/js/main.2d27fc01.chunk.js"
+  },
+  {
+    "revision": "3d74700446f05d9895fe",
+    "url": "/say20-React/static/js/runtime-main.9d093452.js"
+  }
+]);
